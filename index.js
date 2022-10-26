13d693cafe81db51c0bc3b053fec77d66cdbf097
ShowierData9978 <68120127+showierdata9978@users.noreply.github.com>
2022-10-26 23:22:56 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,130 @@
+
+
+const express = require('express');
+var nunjucks = require('nunjucks');
+
+const app = express();
+const fs = require('fs');
+
+var expressWinston = require('express-winston');
+var winston = require('winston');
+
+
+http_codes = {
+    '100': 'Continue',
+    '101': 'Switching Protocols',
+    '102': 'Processing',
+    '103': 'Early Hints',
+    '200': 'OK',
+    '201': 'Created',
+    '202': 'Accepted',
+    '203': 'Non-Authoritative Information',
+    '204': 'No Content',
+    '205': 'Reset Content',
+    '206': 'Partial Content',
+    '207': 'Multi-Status',
+    '208': 'Already Reported',
+    '226': 'IM Used',
+    '300': 'Multiple Choices',
+    '301': 'Moved Permanently',
+    '302': 'Found',
+    '303': 'See Other',
+    '304': 'Not Modified',
+    '305': 'Use Proxy',
+    '307': 'Temporary Redirect',
+    '308': 'Permanent Redirect',
+    '400': 'Bad Request',
+    '401': 'Unauthorized',
+    '402': 'Payment Required',
+    '403': 'Forbidden',
+    '404': 'Not Found',
+    '405': 'Method Not Allowed',
+    '406': 'Not Acceptable',
+    '407': 'Proxy Authentication Required',
+    '408': 'Request Timeout',
+    '409': 'Conflict',
+    '410': 'Gone',
+    '411': 'Length Required',
+    '412': 'Precondition Failed',
+    '413': 'Payload Too Large',
+    '414': 'URI Too Long',
+    '415': 'Unsupported Media Type',
+    '416': 'Range Not Satisfiable',
+    '417': 'Expectation Failed',
+    '418': "I'm a Teapot",
+    '421': 'Misdirected Request',
+    '422': 'Unprocessable Entity',
+    '423': 'Locked',
+    '424': 'Failed Dependency',
+    '425': 'Too Early',
+    '426': 'Upgrade Required',
+    '428': 'Precondition Required',
+    '429': 'Too Many Requests',
+    '431': 'Request Header Fields Too Large',
+    '451': 'Unavailable For Legal Reasons',
+    '500': 'Internal Server Error',
+    '501': 'Not Implemented',
+    '502': 'Bad Gateway',
+    '503': 'Service Unavailable',
+    '504': 'Gateway Timeout',
+    '505': 'HTTP Version Not Supported',
+    '506': 'Variant Also Negotiates',
+    '507': 'Insufficient Storage',
+    '508': 'Loop Detected',
+    '509': 'Bandwidth Limit Exceeded',
+    '510': 'Not Extended',
+    '511': 'Network Authentication Required'
+}
+
+app.use(expressWinston.logger({
+      transports: [
+        new winston.transports.Console()
+      ],
+      meta: false, // optional: control whether you want to log the meta data about the request (default to true)
+      msg: "{{req.method}} {{req.url}} {{req.status}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      expressFormat: false, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+      colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+    }));
+
+
+var imgs = fs.readdirSync(__dirname + '/imgs');
+
+//remove the dir name from the img path
+imgs = imgs.map(function(file) {
+  return file.split("/").pop()
+});
+
+nunjucks.configure(".", {
+  autoescape: true,
+  express: app,
+});
+
+
+app.get('/', (req, res) => {
+
+  res.render('./index.html', {"urls": imgs, "statuscodes":http_codes});
+});
+
+
+app.get('/:code', (req, res) => {
+
+  try {
+    res.sendFile(__dirname + "/imgs/"+ req.params.code.replace(".jpg", "") +".jpg");
+  }  catch (err) {
+    res.status(404).sendFile(__dirname + "/imgs/404.jpeg");
+  }
+});
+
+
+//print every request
+app.on('response', (req, res) => {
+  console.log('Request:'+ req.url);
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+
+});
